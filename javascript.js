eb8e0f07f080ccfff171eeaae0def1e3f78db7ea
Jorge Bencomo <181574375+Obiwan47@users.noreply.github.com>
2025-01-21 18:24:33 -0300

    function getHumanChoice and playRound

--- javascript.js
+++ javascript.js
@@ -1,11 +1,52 @@
 function getComputerChoice() {
 
     const moves = ["ROCK", "PAPER", "SCISSORS"];
-
     return moves[Math.floor(Math.random() * 3)];
 
 }
 
-let vComputer = (getComputerChoice());
+function getHumanChoice() {
 
-document.write(vComputer);
+    const player = prompt("Ingrese su jugada (ROCK - PAPER - SCISSORS)");
+    return player.toUpperCase();
+}
+
+
+function playRound(computerChoice, humanChoice) {
+
+    if(humanChoice === computerChoice) {
+
+        console.log("It's a tie");
+
+    } else if (
+        (humanChoice === "ROCK" && computerChoice === "SCISSORS") ||
+        (humanChoice === "PAPER" && computerChoice === "ROCK") ||
+        (humanChoice === "SCISSORS" && computerChoice === "PAPER")
+    
+    ) {
+
+        humanScore++;
+        console.log("You Win");
+
+    } else {
+
+        computerScore++;
+        console.log("You Lose");
+
+    }
+
+}
+
+let humanScore = 0;
+let computerScore = 0;
+
+const computerSelection = (getComputerChoice());
+const humanSelection = (getHumanChoice());
+
+console.log('Computadora: ' + computerSelection);
+console.log('Jugador: ' + humanSelection);
+
+playRound(computerSelection, humanSelection);
+
+console.log('Player score:' + humanScore);
+console.log('Computer score: ' + computerScore);
